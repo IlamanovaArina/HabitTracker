fd397d92ca1f62f2bab70afd407e65cd5890888a
Arina <ilamanova.arina@gmail.com>
2025-05-12 21:06:55 +0300

    Попытка ...

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = os.getenv('SECRET_KEY') or get_random_secret_key()
 
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = ['158.160.182.163', 'localhost', '127.0.0.1', 'django']
+# ALLOWED_HOSTS = ['158.160.182.163', 'django']
+ALLOWED_HOSTS = ['django', ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
