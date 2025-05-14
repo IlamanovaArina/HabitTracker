76cba50a5e4b3810915e523bea6bed01e61112bd
Arina <ilamanova.arina@gmail.com>
2025-05-14 20:47:17 +0300

    Попытка ...

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
